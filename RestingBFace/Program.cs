cb8606a6d89e73e76632adae6db31611f0c4814c
Alva Svensson <alva.svensson@elev.ga.ntig.se>
2024-01-26 11:28:11 +0100

    26/1

--- RestingBFace/Program.cs
+++ RestingBFace/Program.cs
@@ -2,16 +2,34 @@
 using System.Text.Json;
 using RestSharp;
 
-Console.WriteLine("what artist?");
-string artist = Console.ReadLine();
-Console.WriteLine("what song title?");
-string songTitle = Console.ReadLine();
-RestClient client = new("https://api.lyrics.ovh/v1");
+bool wannaPlay = true;
+while (wannaPlay == true)
+{
 
-RestRequest request = new($"{artist}/{songTitle}");
-RestResponse response = client.GetAsync(request).Result;
+    Console.WriteLine("what artist?");
+    string artist = Console.ReadLine();
+    Console.WriteLine("what song title?");
+    string songTitle = Console.ReadLine();
+    RestClient client = new("https://api.lyrics.ovh/v1");
 
+    RestRequest request = new($"{artist}/{songTitle}");
+    RestResponse response = client.GetAsync(request).Result;
 
-File.WriteAllText("lyric.Json", response.Content.Replace("\\n", "\n"));
-Console.WriteLine(response.Content.Replace("\\n", "\n"));
-Console.ReadLine();
+    if (response.StatusCode == System.Net.HttpStatusCode.OK)
+    {
+        // File.WriteAllText("lyric.Json", response.Content.Replace("\\n", "\n"));
+        Console.WriteLine(response.Content.Replace("\\n", "\n"));
+    }
+    else
+    {
+        Console.WriteLine($"Couldn't find {songTitle} by {artist}.");
+    }
+
+    Console.WriteLine("do you want to search again?");
+    string answer = Console.ReadLine();
+
+    if (answer == "no" || answer == "n")
+    {
+        wannaPlay = false;
+    }
+}
